ae0cd2b6da1ba1e0c57b533a4a4d35400e6db1b1
Hari Mahat <hmahat@gmail.com>
2021-02-10 22:17:12 +0200

    day 19 complete

--- Solutions/day19.js
+++ Solutions/day19.js
@@ -48,12 +48,52 @@ function outFunc() {
 const inf = outFunc();
 console.log(inf);
 
-/* innerTwo = () => {
-  let d = ['html', 'css', 'react'];
-  let c = [];
-  d.forEach((ele) => {
-    el = ele.toUpperCase();
-    c.push(el);
-  });
-  return c;
-}; */
+/** Exercise Level 3 */
+/**
+ * Create a personAccount out function. It has firstname, lastname, incomes, expenses inner variables.
+ *  It has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance
+ * inner functions. Incomes is a set of incomes and its description and expenses is also a
+ * set of expenses and its description.
+ */
+
+function personAccount(fname, lname, income, expense, inc = 500, exp = 200) {
+  const firstname = fname;
+  const lastname = lname;
+  let incomes = income;
+  let expenses = expense;
+
+  totalIncome = () => {
+    return incomes;
+  };
+  totalExpenses = () => {
+    return expenses;
+  };
+
+  addIncome = (inc) => {
+    incomes += inc;
+  };
+  addExpenses = (exp) => {
+    expenses += exp;
+  };
+  accountBalance = () => {
+    let balance = incomes - expenses;
+    return balance;
+  };
+  accountInfo = () => {
+    return `${firstname} ${lastname}\nAccount Balance= ${accountBalance()}`;
+  };
+  return {
+    totalIncome: totalIncome(),
+    totalExpenses: totalExpenses(),
+    addIncome: addIncome(inc),
+    addExpenses: addExpenses(exp),
+    accountBalance: accountBalance(),
+    accountInfo: accountInfo(),
+  };
+}
+
+const PA = personAccount('Hari', 'Mahat', 2000, 500);
+console.log(PA.accountInfo);
+//console.log(personAccount((exp = 200)));
+console.log(PA.totalIncome);
+console.log(PA.totalExpenses);
